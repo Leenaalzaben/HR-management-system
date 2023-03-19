0d9b92ec12c10f537e388f376dcb57f8f77d5f58
leenalzaben <leena.zaben61@gmail.com>
2023-03-19 05:59:26 +0000

    HR-management-system

--- accounting.js
+++ accounting.js
@@ -1,115 +1,78 @@
-'use strict';
-let numberOfEmployeeAdministration = 0;
-let numberOfEmployeeFinance = 0;
-let numberOfEmployeeMarketing = 0;
-let numberOfEmployeeDevelopment = 0;
-let sumAdministration = 0;
-let sumFinance=0;
-let sumMarketing=0;
-let sumDevelopment=0;
-// 1. GetItem (mydata)
-let accountBack = localStorage.getItem('myEmployee');
-//const tableData = JSON.parse(localStorage.getItem('myTableData'));
-// 2. convert from object to string
-let objArray = JSON.parse(accountBack);
-console.log(objArray);
+"use strict";
 
+let key = "myEmployee";
 
-  function department(){ 
-    
-  // department.employees.push(employee);
-  // department.totalSalary += employee.salary;
-  }
-
-//try to create js table
-// let table = document.getElementById("myTable");
-const bodyElement=document.getElementById("body");
-let  table = document.createElement('table');
-// console.log(table);
-let headerRow = table.insertRow();
-console.log(headerRow);
-let departmentHeader = headerRow.insertCell();
-//console.log(departmentHeader);
-  departmentHeader.textContent = 'Department Name';
-  let employeeHeader = headerRow.insertCell();
-  // console.log(employeeHeader);
-  employeeHeader.textContent = 'Number of employees';
-  let totalSalaryHeader = headerRow.insertCell();
-  totalSalaryHeader.textContent = 'Total Salary';
-  // console.log(totalSalaryHeader);
-  let averageHeader = headerRow.insertCell();
-  averageHeader.textContent = 'Average';
-  // console.log(averageHeader);
-  
-
-
-
-
-
-
-
-// Create a new table element
-for (let i = 0; i < objArray.length; i++) {
-  if (objArray[i].department == "Administration") {
-    numberOfEmployeeAdministration++;
-    sumAdministration +=objArray[i].salary;
-    console.log(sumAdministration);
-    
-  }
-  if (objArray[i].department == "Finance") {
-    numberOfEmployeeFinance++;
-    sumFinance+=objArray[i].salary;
-    console.log(sumFinance);
-    
-  }
-  if (objArray[i].department == "Marketing") {
-    numberOfEmployeeMarketing++;
-    sumMarketing+=objArray[i].salary;
-    console.log(sumMarketing);
-    
-  }
-  if (objArray[i].department == "Development") {
-    numberOfEmployeeDevelopment++;
-    sumDevelopment+=objArray[i].salary;
-    console.log(sumDevelopment);
-  }
-  
-  
-  
+function compare(a, b) {
+    if (a.department > b.department)
+        return 1;
+    if (a.department < b.department)
+        return -1;
+    if (a.fullName > b.fullName)
+        return 1;
+    if (a.fullName < b.fullName)
+        return -1;
+    return 0;
 }
-console.log(numberOfEmployeeFinance);
-console.log(numberOfEmployeeMarketing);
-console.log(numberOfEmployeeAdministration);
-
-function render() {
-  let tableRow=document.createElement("tr")
-  bodyElement.appendChild(tableRow);
-  // console.log(tableRow);
-  
-  
-  let tableDataRow =document.createElement("td");
-  console.log(tableDataRow);
-  tableDataRow.textContent=objArray[i].department;
-  
-  
-  
-  
-  
-  
-  
-  //tr
-  //appendtable
-  //td
-  // Retrieve data from local storage
-  
-  
-  
-
-  
-  
-
-
+// 1. GetItem mydata key
+function getData(key) {
+    let accountBack = localStorage.getItem(key);
+    return JSON.parse(accountBack);
 }
 
 
 
+//2. To add the row for table 
+function addTableRow(compartmentId, departmentType, numOfEmployees, averageSalary, salarySum) {
+    let row = compartmentId.insertRow();
+    let cell = row.insertCell();
+    cell.innerHTML = departmentType;
+    cell = row.insertCell();
+    cell.innerHTML = numOfEmployees;
+    cell = row.insertCell();
+    cell.innerHTML = averageSalary;
+    cell = row.insertCell();
+    cell.innerHTML = salarySum;
+}
+
+
+
+function genTable(allEmployees) {
+    let numOfEmployees = 0;
+    let salarySum = 0;
+    let totalSalary = 0;
+    let totalEmployees = 0;
+    let departmentType = allEmployees[0].department;
+    // get element by body,footer
+    let tableBody = document.getElementById("tableBody");
+    let tableFoot = document.getElementById("tableFoot");
+
+    for (let index = 0; index < allEmployees.length; index++) {
+        if (departmentType == allEmployees[index].department) {
+            numOfEmployees++;
+            salarySum += parseFloat(allEmployees[index].salary);
+        }
+        else {
+            addTableRow(tableBody, departmentType, numOfEmployees, salarySum / numOfEmployees, salarySum);
+            totalSalary += parseFloat(salarySum);
+            totalEmployees += numOfEmployees;
+            numOfEmployees = 1;
+            salarySum = parseFloat(allEmployees[index].salary);
+            departmentType = allEmployees[index].department;
+        }
+        if (index == allEmployees.length - 1) {
+            addTableRow(tableBody, departmentType, numOfEmployees, salarySum / numOfEmployees, salarySum);
+            totalSalary += parseFloat(salarySum);
+            totalEmployees += parseFloat(numOfEmployees);
+        }
+    }
+    // myfooter 
+    addTableRow(tableFoot, "Summry", totalEmployees, totalSalary / totalEmployees, totalSalary);
+}
+
+
+
+window.onload = function () {
+    let allEmployees = getData(key);
+    allEmployees.sort(compare);
+    genTable(allEmployees);
+}
